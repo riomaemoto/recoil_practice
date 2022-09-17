619150b76c85cc7dd02c3485760316210e9b849c
enu <enu1996502@gmail.com>
2022-09-18 04:23:34 +0900

    削除とトグル　追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button, Checkbox, Flex, Input, Text } from "@chakra-ui/react";
 import { useState } from "react";
 import { useRecoilState } from "recoil";
 import { todolistState } from "./global/state";
@@ -17,21 +18,48 @@ const App = () => {
     setInputText("");
   };
 
+  const onDelete = (a: number) => {
+    const item: Todo[] = data.filter((todo) => todo.id !== a);
+    setData(item);
+  };
+
+  const onToggle = (i: number) => {
+    const item: Todo = {
+      ...data[i],
+      isDone: !data[i].isDone,
+    };
+    const items: Todo[] = [...data.slice(0, i), item, ...data.slice(i + 1)];
+    setData(items);
+  };
+
   return (
     <div style={{ padding: "20px" }}>
-      <input
-        type="text"
-        value={inputText}
-        onChange={(e) => setInputText(e.target.value)}
-      />
-      <button onClick={onSubmit}>ボタン</button>
-      <br />
-      <br />
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>{item.content}</li>
-        ))}
-      </ul>
+      <Flex mb={8}>
+        <Input
+          width={"50vw"}
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          mr={4}
+        />
+        <Button onClick={onSubmit}>ボタン</Button>
+      </Flex>
+      {data.map((item, index) => (
+        <Flex key={item.id} mb={4}>
+          <Checkbox
+            isChecked={item.isDone}
+            onChange={() => onToggle(index)}
+            mr={4}
+          />
+          <Text pt={2}>{item.content}</Text>
+          <Button
+            ml={4}
+            onClick={() => onDelete(item.id)}
+            disabled={!item.isDone}
+          >
+            削除
+          </Button>
+        </Flex>
+      ))}
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <ChakraProvider>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
